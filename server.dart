f16d99af1c138ab018a8906cfa26da6237d5f3db
Mike Plavsky <Mike.Plavsky@quest.com>
2013-10-25 22:21:02 +0000

    closure

--- server.dart
+++ server.dart
@@ -1,3 +1,10 @@
+makeAdder(var addBy) {
+  adder(var i) {
+    return i + addBy;
+  }
+  return adder;
+}
+
 var l = [1,2,3,4];
 var g = {"test": 1, "next": 2, "prev": "a"};
 
@@ -6,5 +13,7 @@ main() {
   print("Yep!");
   print(l);
   print(g);
+
+  print(makeAdder(12)(2));
 
 }

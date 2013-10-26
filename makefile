6e9683122ccdcf9d1edb60f780616a7cec0b77a9
Mike Plavsky <Mike.Plavsky@quest.com>
2013-10-26 17:03:19 +0000

    makefile

---
+++ makefile
@@ -0,0 +1,2 @@
+all:
+	dart server.dart
--- server.dart
+++ server.dart
@@ -16,4 +16,15 @@ main() {
 
   print(makeAdder(12)(2));
 
+  var s = "some string";
+
+  var splitClosure1 = s.split;
+  var splitClosure2 = s.split;
+
+  print( splitClosure1 );
+  print( makeAdder );
+
+  print( splitClosure1 == splitClosure2 );
+  print( s.split == s.split );
+
 }
